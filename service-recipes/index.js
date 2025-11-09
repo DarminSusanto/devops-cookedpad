2454ef103a8dfe6cf12819921f629eb02fd950dc
Darmin Susanto <darminsusanto39@gmail.com>
2025-11-09 13:24:42 +0700

    Hapus beberap command dlm kode

--- service-recipes/index.js
+++ service-recipes/index.js
@@ -3,13 +3,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const Recipe = require('./models/Recipe'); // Import model Recipe
-const auth = require('./auth'); // Import "Penjaga Gerbang" (Middleware)
+const Recipe = require('./models/Recipe'); 
+const auth = require('./auth');
 
 const app = express();
 const PORT = 3002;
 app.use(express.json());
-app.use(cors()); // Terapkan CORS untuk semua rute
+app.use(cors()); 
 
 // --- Koneksi Database ---
 const DB_URI = 'mongodb://mongo-db:27017/db_resep';
